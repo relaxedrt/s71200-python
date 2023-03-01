20e3b0b748cb2b9079d5ed7b4b2e69cd27301144
relaxedrt <rodrigotejeria01@gmail.com>
2023-03-01 16:31:45 +0100

    prueba programada

--- pruebas.py
+++ pruebas.py
@@ -2,6 +2,10 @@ import siemens
 import time
 
 plc = siemens.comm("192.168.0.1", 0, 1)
-for i in range(0,50):
-    plc.write_int(1,8,i)
-    time.sleep(0.5)
+while True:
+    if plc.db_read_bool(1,10,1) == False:
+        print("Esperando disparo de plc")
+    if plc.db_read_bool(1,10,1) == True:
+        for i in range(0,50):
+            plc.write_int(1,8,i)
+            time.sleep(0.5)
