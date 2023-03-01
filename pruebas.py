f07741222462b74ac6c160d7eb3f4ffca728c473
relaxedrt <rodrigotejeria01@gmail.com>
2023-03-01 16:56:46 +0100

    s

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,11 @@
 import siemens
 import time
 
+#Hacemos la conexión con el plc
 plc = siemens.comm("192.168.0.1", 0, 1)
+#Creamos un bucle de programa
 while True:
+    #Consultamos el valor de la variable de disparo
     if plc.db_read_bool(1,10,1) == False:
         print("Esperando disparo de plc")
     if plc.db_read_bool(1,10,1) == True:
